213a69211ad153e3902d14c9ae99fe7352a83995
dingjules <dingjules@gmail.com>
2017-11-12 22:08:15 +0100

    add dynamic color

--- app/services/DataHelper.service.js
+++ app/services/DataHelper.service.js
@@ -1,3 +1,6 @@
+import randomColor from 'randomcolor';
+import Color from 'chartjs-color';
+
 import audience from '../data/audience.json';
 import bandwidth from '../data/bandwidth.json';
 import country from '../data/country.json';
@@ -6,10 +9,22 @@ import platform from '../data/platform.json';
 import streams from '../data/streams.json';
 import usage from '../data/usage.json';
 
+const getRandomColorSet = () => {
+    const color = randomColor({
+        format: 'rgba',
+        alpha: 1
+    });
+    return {
+        boderColor: color,
+        backgroundColor: Color(color).clearer(0.8).rgbString()
+    };
+};
+
 export const formateTimeRelatedData = data => {
     const res = [];
     Object.keys(data).forEach(key => {
         const dataSet = [];
+        const colorSet = getRandomColorSet();
         data[key].map(item => {
             dataSet.push({
                 x: item[0],
@@ -19,7 +34,9 @@ export const formateTimeRelatedData = data => {
         res.push(
             {
                 label: key,
-                data: dataSet
+                data: dataSet,
+                backgroundColor: colorSet.backgroundColor,
+                borderColor: colorSet.borderColor
             }
         );
     });
@@ -29,23 +46,30 @@ export const formateTimeRelatedData = data => {
 export const formatData = (data, labelKey) => {
     const res = {
         labels: [],
-        datasets: []
+        datasets: [],
+        backgroundColor: [],
+        borderColor: []
     }, dataByCatgory = {};
     data.map(item => {
         res.labels.push(item[labelKey]);
         Object.keys(item).forEach(key => {
             if (key !== labelKey) {
-                dataByCatgory[key] = dataByCatgory[key] || [];
-                dataByCatgory[key].push(item[key]);
+                dataByCatgory[key] = dataByCatgory[key] || { data: [], backgroundColor: [], borderColor: [] };
+                dataByCatgory[key].data.push(item[key]);
+                const colorSet = getRandomColorSet();
+                dataByCatgory[key].backgroundColor.push(colorSet.backgroundColor);
+                dataByCatgory[key].borderColor.push(colorSet.boderColor);
             }
         });
     });
-    
+
     Object.keys(dataByCatgory).forEach(key => {
         res.datasets.push(
             {
                 label: key,
-                data: dataByCatgory[key]
+                data: dataByCatgory[key].data,
+                backgroundColor: dataByCatgory[key].backgroundColor,
+                borderColor: dataByCatgory[key].borderColor
             }
         );
     });
